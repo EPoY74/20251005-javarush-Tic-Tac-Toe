5871dbba69b15bb6ce8bd9b4cc1dba612cb0d1d6
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-10-06 19:51:35 +0400

    Initial commit too

---
+++ src/com/javarush/games/ticktacktoe/Main.java
@@ -0,0 +1,72 @@
+package com.javarush.games.ticktacktoe;
+
+import com.javarush.engine.cell.*;
+
+public class TicTacToeGame extends Game {
+    private int[][] model = new int[3][3];
+    private int currentPlayer;
+
+    public void initialize(){
+        setScreenSize(3,3);
+        startGame();
+        updateView();
+    }
+
+    public void startGame(){
+        for (int i = 0; i < model.length; i++){
+            for (int j = 0; j < model[i].length; j++){
+                model[i][j] = 0;
+            }
+        }
+        currentPlayer = 1;
+    }
+
+
+    public void updateCellView( int x, int y, int value){
+        String displayedValue = null;
+        Color displayedColor = Color.WHITE;
+
+        //setCellValueEx(x, y, Color.WHITE, displayedValue, Color.WHITE);
+        if (value == 0){
+            displayedValue = " ";
+        }
+        else if (value == 1){
+            displayedValue = "X";
+            //setCellValueEx(x, y, Color.WHITE, displayedValue, Color.RED);
+            displayedColor = Color.RED;
+        }
+        else if (value == 2){
+            displayedValue = "O";
+            //setCellValueEx(x, y, Color.WHITE, displayedValue, Color.BLUE);
+            displayedColor = Color.BLUE;
+        }
+        else{
+            displayedValue = null;
+        }
+        if (displayedValue == null){
+            System.out.println("Ошибка значения value: " + value);
+        }
+        setCellValueEx(x, y, Color.WHITE, displayedValue, displayedColor);
+
+    }
+
+    public void updateView(){
+        for (int i = 0; i < model.length; i ++){
+            for (int j = 0; j < model[i].length; j++){
+                updateCellView(i, j, model[i][j]);
+            }
+        }
+    }
+
+    /**
+     * Called when the user clicks on the game field with the mouse.
+     * @param x :Position (coordinate) on the game field
+     * @param y :Position (coordinate) on the game field
+     */
+    public void onMouseLeftClick(int x, int y){
+        model[x][y] = currentPlayer;
+        updateView();
+        currentPlayer = 3 - currentPlayer;
+    }
+
+}
